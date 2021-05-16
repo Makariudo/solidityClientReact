e4d10ea9be513e46b8f176fc7b8b36c9eab0281c
Makariudo <sspassevitch@gmail.com>
2021-05-16 21:56:44 +0200

    test ok

--- contracts/MindPin.sol
+++ contracts/MindPin.sol
@@ -15,11 +15,15 @@ contract MindPin is Ownable {
     uint id;
     address owner;
     string content;
+    bool exists;
   }
 
+  //2 mappings un pour avoir l'ensemble des states peu importe le owner et l'autre cf owner
   mapping(address => mapping(uint => State)) public bookStates;
   mapping(uint => State) public NumStates;
-    
+  
+  //nombre d'entrées par owner
+  mapping(address => uint16) public AddressEntries;
 
 //events
 event MindSet(
@@ -28,9 +32,28 @@ event MindSet(
   string content
 );
 
+event DeleteMindSet(
+  address owner,
+  uint id,
+  string content
+);
+
+event UpdateMindSet(
+  address owner,
+  uint id,
+  string newContent,
+  string oldContent,
+  string message
+);
+
 // modifier
 modifier isOwner(State memory entry) {
   require(msg.sender == entry.owner, "Caller is not the owner");
+  _;
+}
+
+modifier isOkToUpdate(uint _id) {
+  require(NumStates[_id].exists   , "No state to update here !");
   _;
 }
 
@@ -39,25 +62,54 @@ modifier isOwner(State memory entry) {
     /** 
      * @dev Create a new Mindset 
      * @param _content - MindesetContent
-     * @return _id - Returns mindset Id
+     * @return id - Returns mindset Id
      */
-    function writeState(string memory _content) external returns (uint _id) {
+    function writeState(string memory _content) external returns (uint id) {
         counter++;
-        State memory entry = State(counter, msg.sender, _content);
+        State memory entry = State(counter, msg.sender, _content, true);
         NumStates[counter] = entry;
         bookStates[msg.sender][counter] = entry;
+        AddressEntries[msg.sender] = AddressEntries[msg.sender]+1;
         emit MindSet(msg.sender, counter, _content);
         return (counter);
     }
 
     /** 
      * @dev getOneState
-     * @param _id - id du State
-     * @return _result - Returns state
+     * @param _id - State's id
+     * @return result - Returns state
      */
-    function getState(uint _id) public view returns (State memory _result) {
+    function getState(uint _id) public view returns (State memory result) {
       return NumStates[_id];
     }
 
-    ///TODO get/Update/delete
+     /** 
+     * @dev deleteOneState
+     * @param _id - State's id
+     * @return message - Returns confirm message
+     */
+    function deleteState(uint _id) public isOwner(getState(_id)) returns (string memory message) {
+      NumStates[_id].content = "";
+      bookStates[msg.sender][_id].content = "";
+      NumStates[_id].exists = false;
+      bookStates[msg.sender][_id].exists = false;
+      string memory _message = "State erased";
+      emit DeleteMindSet(msg.sender, _id, _message);
+      return _message;
+    }
+
+    /** 
+     * @dev updateState
+     * @param _id - State's id
+     * @param _content - State's content to update
+     * @return message - Returns confirm message
+     */
+    function updateState(uint _id, string memory _content) public isOwner(getState(_id)) isOkToUpdate(_id) returns (string memory message) {
+      string memory oldStateContent = NumStates[_id].content;
+      NumStates[_id].content =  _content;
+      bookStates[msg.sender][_id].content = _content;
+      string memory _message = "State updated!";
+      emit UpdateMindSet(msg.sender, _id, _content, oldStateContent, _message);
+      return _message;
+    }
 }
--- test/MindPin.test.js
+++ test/MindPin.test.js
@@ -20,9 +20,10 @@ describe('Deploy MindPin contract and test', () => {
       expect(mindPinInstance.address).to.exist;
     });
     ///AAA pattern Arrange/Act/Assert
-    describe('Test writing function', () => {
+    describe('Test writing function', async () => {
       let result;
       let _content = "Aujourd'hui, je me sens bien, l'air est frais et je vais pouvoir en profiter!";
+      let _content2 = "deuxieme contenu à tester"
 
       it("should create a state id 1", async () => {
         result = await mindPinInstance.writeState(_content, {from: toto});
@@ -30,9 +31,48 @@ describe('Deploy MindPin contract and test', () => {
         expect(mapping.id).to.bignumber.equal(new BN(1));
         //expect(receipt.status).to.be.true;
       })
+
       it("should emit an event", async () => {
         //args dans l'objet en troisième param
         expectEvent(result, "MindSet", { owner: toto, content: _content});
+      })
+
+      it("should increment AddressEntries", async () => {
+        const res = await mindPinInstance.AddressEntries.call(toto)
+        expect(res).to.bignumber.equal(new BN(1));
+      })
+
+      it("should return a state", async () => {
+        const res = await mindPinInstance.getState(1);
+        expect(res.content).to.be.equal(_content);
+      })
+
+      it("should create a second state", async () => {
+        await mindPinInstance.writeState(_content2, {from: toto});
+        const res = await mindPinInstance.getState(2);
+        expect(res.content).to.be.equal(_content2);
+      })
+
+      it("should block the delete of this second state", async () => {
+        await expectRevert(mindPinInstance.deleteState(2, {from: david}), "Caller is not the owner");
+      })
+
+      it("should delete this second state", async () => {
+        await mindPinInstance.deleteState(2, {from: toto});
+        const res = await mindPinInstance.getState(2);
+        expect(res.content).to.be.empty;
+      })
+
+      it("should update this first state", async () => {
+        const updater = await mindPinInstance.updateState(1, _content2, {from: toto});
+        const res = await mindPinInstance.getState(1);
+        expect(res.content).to.be.equal(_content2);
+        expectEvent(updater, "UpdateMindSet", { owner: toto, newContent: _content2, oldContent: _content, message: "State updated!"});
+      })
+
+      it("should block this update", async () => {
+        const res =  await mindPinInstance.NumStates.call(2);
+        await expectRevert(mindPinInstance.updateState(2, _content2, {from: toto}), "No state to update here !");
       })
     })
 })
