976ba55ec36552a93237e025c9be51aaa82924a3
Makariudo <sspassevitch@gmail.com>
2021-05-17 00:34:42 +0200

    dispatch action own reducer with drizzle okay!

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
-import { Container } from '@material-ui/core';
+import {useState} from 'react'; 
+import { generateStore, Drizzle } from '@drizzle/store';
+import { Container, Box } from '@material-ui/core';
+import { drizzleReactHooks} from '@drizzle/react-plugin';
+import {changeName} from './redux/action';
 import Header from "./Header.js";
 import './App.css';
 
+
 function App() {
+  const { useDrizzle, useDrizzleState } = drizzleReactHooks;
+  const drizzleState = useDrizzleState(state => ({...state}));
+  const {drizzle} = useDrizzle();
+
+  const [input, setInput] = useState('init');
+  const handleChange = (event) => {
+    setInput(event.target.value)
+  }
+  const handleSubmit = () => {
+    drizzle.store.dispatch(changeName(input))
+  }
   return (
-    <Container className="App" maxWidth="sm">
+    <Container className="App" maxWidth="m">
       <Header />
+      <Box>
+        <input type='text' value={input} onChange={handleChange}></input>
+        <button onClick={handleSubmit}>Click change name</button>
+      </Box>
+
     </Container>
   );
 }
--- client/src/Header.js
+++ client/src/Header.js
@@ -2,7 +2,7 @@ import { AppBar } from '@material-ui/core';
 
 function Header() {
   return (
-    <AppBar color="primary" style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems:'center'}}>
+    <AppBar color="primary" style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems:'center', position: 'relative', width:'100%'}}>
       <h1 style= {{marginLeft: '6rem'}}>Bienvenue sur l'appli MindPin</h1>
       <h3 style= {{marginRight: '2rem'}} > Address: 0101sdf02sd0f2s0df</h3>
     </AppBar>
